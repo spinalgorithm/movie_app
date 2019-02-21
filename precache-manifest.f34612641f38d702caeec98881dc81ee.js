21d6bdcf273fecc135716721f42ab2cdececa528
spinalgorithm <spinalgorithm@gmail.com>
2019-02-21 16:43:45 +0900

    Updates

---
+++ precache-manifest.f34612641f38d702caeec98881dc81ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "6cde8b77c6b3f4376d50",
+    "url": "/movie_app/static/js/main.6cde8b77.chunk.js"
+  },
+  {
+    "revision": "0a935515e47631cc43e9",
+    "url": "/movie_app/static/js/1.0a935515.chunk.js"
+  },
+  {
+    "revision": "6cde8b77c6b3f4376d50",
+    "url": "/movie_app/static/css/main.0d65bd58.chunk.css"
+  },
+  {
+    "revision": "dc37a70f80593daeddceeb07f26e9916",
+    "url": "/movie_app/index.html"
+  }
+];
